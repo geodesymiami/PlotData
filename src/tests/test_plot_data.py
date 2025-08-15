665d1097691523de932bc51c1269b5048b2bb44a
disilvestro <ds96@live.it>
2025-08-14 22:38:39 -0400

    test_plot_data.py: to configure test later

--- src/tests/test_plot_data.py
+++ src/tests/test_plot_data.py
@@ -3,8 +3,9 @@ from plotdata.objects.plotters import VelocityPlot
 
 class TestVelocityPlot(unittest.TestCase):
     def test_velocity_plot(self):
-        plot = VelocityPlot()
-        self.assertIsNotNone(plot)
+            pass
+            plot = VelocityPlot()
+            self.assertIsNotNone(plot)
 
 if __name__ == "__main__":
     unittest.main()
