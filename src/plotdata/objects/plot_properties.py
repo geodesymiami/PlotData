f0b94ace2856814a214b9dc0a06b466964c63f60
disilvestro <ds96@live.it>
2025-09-18 17:47:50 -0400

    plot_properties.py: Changed width of figure based on columns

--- src/plotdata/objects/plot_properties.py
+++ src/plotdata/objects/plot_properties.py
@@ -97,6 +97,7 @@ class PlotRenderer:
 class PlotGrid:
     def __init__(self, template: PlotTemplate, inps):
         self.template = template
+        self.num_columns = int(max(len(row) for row in template.layout))
         if inps.flag_save_axis:
             self.axes = self._create_figures(inps)
         else:
@@ -123,9 +124,14 @@ class PlotGrid:
         return axs
 
     def _create_axes(self, inps):
+        width = {
+            1: 6,
+            2: 10,
+            3: 20,
+        }
         fig, axs = plt.subplot_mosaic(
             self.template.layout,
-            figsize=(20, 9),
+            figsize=(width[self.num_columns], 9),
             constrained_layout=self.template.constrained_layout,
         )
 
