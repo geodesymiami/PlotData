2a91390f4c6521f8512084a847bee815d6dca935
disilvestro <ds96@live.it>
2025-11-26 15:45:30 -0500

    plotters.py: re-introduced geometry file

--- src/plotdata/helper_functions.py
+++ src/plotdata/helper_functions.py
@@ -2,15 +2,44 @@
 import os
 import sys
 import math
-import subprocess
 import glob
-from mintpy.utils import readfile
-from mintpy.objects import HDFEOS
-from scipy.interpolate import interp1d
+import subprocess
 import numpy as np
 from pathlib import Path
-from mintpy.utils import utils, writefile
+from scipy.ndimage import zoom
+from mintpy.utils import readfile
+from mintpy.objects import HDFEOS
 from mintpy.cli import generate_mask
+from scipy.interpolate import interp1d
+from mintpy.utils import utils, writefile
+
+
+def resize_to_match(target, reference, name):
+    """
+    Resize the target array to match the shape of the reference array.
+
+    Parameters:
+    target (ndarray): The array to be resized.
+    reference (ndarray): The array whose shape will be matched.
+    name (str): A name for the target array, used in error messages.
+
+    Returns:
+    ndarray: The resized target array if the shapes do not match, otherwise the original target array.
+
+    Raises:
+    ValueError: If the target array has an invalid shape (i.e., any dimension is non-positive).
+    """
+
+    if target.shape != reference.shape:
+        if all(dim > 0 for dim in target.shape):
+            zoom_factors = (
+                reference.shape[0] / target.shape[0],
+                reference.shape[1] / target.shape[1],
+            )
+            return zoom(target, zoom_factors, order=1)
+        else:
+            raise ValueError(f"Invalid shape for {name}: {target.shape}")
+    return target
 
 
 def extract_temporalCoherence(file_path, coords):
