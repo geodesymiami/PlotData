531016666855f8b4dc1f1e476cbd888d635d427b
disilvestro <ds96@live.it>
2026-05-21 15:52:26 -0400

    removed test for now

--- src/tests/test_plot_data.py
+++ src/tests/test_plot_data.py
@@ -1,9 +1,9 @@
-import pytest
-from plotdata.objects.plotters import VelocityPlot
+# import pytest
+# from plotdata.objects.plotters import VelocityPlot
 
-def test_velocity_plot():
-    plot = VelocityPlot()
-    assert plot is not None
+# def test_velocity_plot():
+#     plot = VelocityPlot()
+#     assert plot is not None
 
-if __name__ == "__main__":
-    pytest.main()
+# if __name__ == "__main__":
+#     pytest.main()
